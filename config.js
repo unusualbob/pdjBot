9ad00df0e2e121bca4eca66c613398e4ecc13929
Rob Riddle <unusualbob@gmail.com>
2012-10-16 03:04:11 -0400

    move config variables into their own file for ease of git

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    username : '' //TWITTER USERNAME GOES HERE
+  , password : '' //TWITTER PASSWORD GOES HERE
+  , adminIds : ["14022630"] //Array of users who have access to admin commands
+}
