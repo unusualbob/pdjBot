d3edbfd55b901b6e0bb3d299fb4b2c06e76be54a
Eric Martindale <eric@ericmartindale.com>
2012-11-08 15:10:25 -0500

    Fix bug with infix commands.

--- bot.js
+++ bot.js
@@ -77,6 +77,7 @@ phantom.create(function(ph) {
                   var tokens = data.message.split(" ");
                   tokens.forEach(function(token) {
                     if (token.substr(0, 1) === '!') {
+                      data.message('/'+token);
                       command(data);
                     }
                   });
